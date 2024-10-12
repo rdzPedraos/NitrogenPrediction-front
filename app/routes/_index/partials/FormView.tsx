b30ebe5132afb4395a7b06eb51b97d73b1c43d94
rdzPedraos <rdzpedraos@gmail.com>
2024-10-11 21:09:17 -0500

    feat: add form view

---
+++ app/routes/_index/partials/FormView.tsx
@@ -0,0 +1,115 @@
+import { Input } from "@nextui-org/react";
+import { Reorder } from "framer-motion";
+import { Upload } from "lucide-react";
+
+import { useFormContext } from "@/contexts/FormContext";
+import { useStepperContext } from "@/contexts/StepperContext";
+import { NextButton, RenderSquare, UploadFileButton } from "@/components";
+import Title from "@/components/Title";
+
+export default function FormView() {
+    const { next } = useStepperContext();
+    const { images, BandTypes, register, alreadyUploadImages } =
+        useFormContext();
+
+    return (
+        <>
+            <Title>Ingreso de variables</Title>
+            <p className="mb-8">
+                Para usar este modelo se requiere cargar una imagen tomada con
+                una cámara multiespectral del terreno a estudiar. Las cámaras
+                multiespectrales te permiten obtener la información en 5
+                canales, por favor, cargue dicha información y posicionelo
+                correctamente, lo mismo para los paneles de refractancia. Y
+                adjunte la información solicitada a continuación.
+            </p>
+
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col md:flex-row gap-5">
+                    <Input
+                        label="Niveles de clorofila"
+                        {...register("clorofila")}
+                        type="number"
+                    />
+                    <Input
+                        label="Fecha inicial del cultivo"
+                        {...register("startDate")}
+                        type="date"
+                    />
+                    <Input
+                        label="Fecha de la toma de muestras (fotos)"
+                        {...register("captureDate")}
+                        type="date"
+                    />
+                </div>
+
+                <div className="flex flex-wrap justify-end gap-2">
+                    <UploadFileButton
+                        onUpload={images.multispectral.upload}
+                        color="primary"
+                        endContent={<Upload width={20} />}
+                    >
+                        Subir imágenes
+                    </UploadFileButton>
+
+                    <UploadFileButton
+                        onUpload={images.refractance.upload}
+                        color="primary"
+                        variant="flat"
+                        endContent={<Upload width={20} />}
+                    >
+                        Subir imágenes panel de refractancia
+                    </UploadFileButton>
+                </div>
+
+                <div className="flex flex-col gap-6">
+                    <div className={`flex gap-4`}>
+                        {BandTypes.map((column) => (
+                            <p
+                                key={column}
+                                className="uppercase text-center font-bold flex-1"
+                            >
+                                {column}
+                            </p>
+                        ))}
+                    </div>
+
+                    {Object.values(images).map(
+                        ({ setImages, images, removeImage }, i) => (
+                            <Reorder.Group
+                                key={i}
+                                axis="x"
+                                onReorder={setImages}
+                                values={images}
+                                className="flex gap-6"
+                            >
+                                {images.map((image) => (
+                                    <Reorder.Item
+                                        key={image.id}
+                                        value={image}
+                                        className="flex-1"
+                                    >
+                                        <RenderSquare
+                                            {...image}
+                                            onRemove={removeImage}
+                                        />
+                                    </Reorder.Item>
+                                ))}
+                            </Reorder.Group>
+                        )
+                    )}
+                </div>
+
+                <div>
+                    <NextButton
+                        onClick={next}
+                        isDisabled={!alreadyUploadImages}
+                        className="float-right"
+                    >
+                        Procesar
+                    </NextButton>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,52 @@
+import { Progress } from "@nextui-org/react";
+import type { MetaFunction } from "@remix-run/node";
+
+import AIProvider from "@/contexts/FormContext";
+import StepperProvider, { useStepperContext } from "@/contexts/StepperContext";
+import { FormView } from "./partials";
+
+export const meta: MetaFunction = () => {
+    return [
+        { title: "Prediccion de nitrogeno" },
+        { name: "description", content: "Prediccion de nitrogeno en cultivos" },
+    ];
+};
+
+export default function Index() {
+    return (
+        <StepperProvider>
+            <AIProvider>
+                <RenderProgress />
+                <div className="max-w-6xl px-4 mx-auto my-16">
+                    <RenderComponent />
+                </div>
+            </AIProvider>
+        </StepperProvider>
+    );
+}
+
+function RenderProgress() {
+    const { step } = useStepperContext();
+    const percentage = ((step + 1) * 100) / 3;
+
+    return (
+        <Progress
+            aria-label="Loading..."
+            value={percentage}
+            radius="none"
+            classNames={{
+                base: "fixed top-0 w-full z-50",
+                track: "bg-default-300/90 h-4",
+                indicator: "bg-gradient-primary",
+            }}
+        />
+    );
+}
+
+function RenderComponent() {
+    const { step } = useStepperContext();
+
+    return {
+        0: <FormView />,
+    }[step];
+}
