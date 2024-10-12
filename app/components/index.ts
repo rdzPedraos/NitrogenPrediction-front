afd548fdb750e75b3f9f91f0f61d99cfce1e03a2
rdzPedraos <rdzpedraos@gmail.com>
2024-10-11 21:06:43 -0500

    feat: add barrier for components

---
+++ app/components/index.ts
@@ -0,0 +1,4 @@
+export { default as RenderSquare } from "./RenderSquare";
+export { default as UploadFileButton } from "./UploadFileButton";
+export { default as NextButton } from "./NextButton";
+export { default as PrevButton } from "./PrevButton";
