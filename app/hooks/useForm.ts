1f67d49a6e3ae43408ee5a6e906711d14eba2ae5
rdzPedraos <rdzpedraos@gmail.com>
2024-10-11 20:43:31 -0500

    feat: add hook useForm for state in forms

---
+++ app/hooks/useForm.ts
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function useForm<T extends object>(initValues: T) {
+    const [form, setForm] = useState<T>(initValues);
+
+    const setData = (key: keyof T, value: any) => {
+        setForm((prev) => ({ ...prev, [key]: value }));
+    };
+
+    const register = (key: keyof T) => {
+        return {
+            value: form[key],
+            onValueChange: (value: any) => setData(key, value),
+        };
+    };
+
+    return {
+        data: form,
+        setData,
+        register,
+    };
+}
+
+export default useForm;
