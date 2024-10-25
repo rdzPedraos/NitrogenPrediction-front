c53523a0c76ccec2bde7210c8047c9b7cf040960
rdzPedraos <rdzpedraos@gmail.com>
2024-10-24 19:06:52 -0500

    fix: type for form

--- app/types/models.ts
+++ app/types/models.ts
@@ -8,9 +8,21 @@ type FilterImage = {
 };
 
 type BasicForm = {
-    clorofila: number;
-    startDate: CalendarDate;
-    captureDate: CalendarDate;
+    session_id: string | null;
+
+    roi_coordinates: {
+        x1: number;
+        y1: number;
+        x2: number;
+        y2: number;
+    };
+
+    data_iot: {
+        soil_humedity: number;
+        soil_temperature: number;
+        pH: number;
+        avg_spad: number;
+    };
 };
 
 type NitrogenPredict = {
