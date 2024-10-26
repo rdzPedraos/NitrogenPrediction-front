2765f6363db4aee82ab44c3484869f5035064b23
rdzPedraos <rdzpedraos@gmail.com>
2024-10-26 00:28:29 -0500

    fix: remove height statement for show all components in menu config

--- app/routes/_index/partials/Drawable.tsx
+++ app/routes/_index/partials/Drawable.tsx
@@ -35,9 +35,9 @@ export default function Drawable() {
                 collapsedWidth="0px"
                 className="overflow-hidden shadow"
             >
-                <div className="overflow-y-auto h-full p-4 flex flex-col gap-4">
+                <div className="overflow-y-auto p-4 flex flex-col gap-4">
                     <div className="flex justify-between items-center">
-                        <h2 className="text-xl font-bold">Configuración</h2>
+                        <h2 className="text-xl font-bold">{t("title")}</h2>
                         <Button
                             onClick={() => setIsOpen(false)}
                             isIconOnly
