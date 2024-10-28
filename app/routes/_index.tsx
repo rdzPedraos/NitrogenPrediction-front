b6703d4a9733297a7ccd2b4117a60e377e832bac
rdzPedraos <rdzpedraos@gmail.com>
2024-10-27 22:26:17 -0500

    fix: move predict to route /predict

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function route() {
+    return (
+        <div className="h-screen grid grid-cols-2">
+            <div>a</div>
+
+            <div>Hi!</div>
+        </div>
+    );
+}
